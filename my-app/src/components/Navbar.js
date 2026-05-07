7e54df8092a8f768694aeafbcf25ca42aeb9b12a
Manish kumar <mrjackel88@gmail.com>
2026-05-07 13:14:44 +0530

    updated css and navbar

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -7,18 +7,23 @@ export default function Navbar() {
 
   return (
     <nav>
-      <Link to="/">Home</Link> |{" "}
-      {token ? (
-        <>
-          <Link to="/bookmarks">Bookmarks</Link> |{" "}
-          <button onClick={logout}>Logout</button>
-        </>
-      ) : (
-        <>
-          <Link to="/login">Login</Link> |{" "}
-          <Link to="/register">Register</Link>
-        </>
-      )}
+      <div className="nav-brand">
+        <Link to="/">HN Scraper</Link>
+      </div>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        {token ? (
+          <>
+            <Link to="/bookmarks">Bookmarks</Link>
+            <button className="nav-logout" onClick={logout}>Logout</button>
+          </>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </div>
     </nav>
   );
 }
